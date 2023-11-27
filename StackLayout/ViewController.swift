90584adb105a4dc2dbd14aab2f61139490809c9b
Sinan Demirci <114944133+snndmrc@users.noreply.github.com>
2023-11-27 14:51:21 +0300

    Initial Commit

---
+++ StackLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackLayout
+//
+//  Created by Sinan Demirci on 2.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
